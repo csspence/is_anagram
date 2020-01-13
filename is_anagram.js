38e79b7911efe15b60f7ec033c78c6b3f9b7d934
Crew Spence <csspence123@gmail.com>
2020-01-13 11:10:04 -0600

    Wrote naive working solution for is_anagram.

--- is_anagram.js
+++ is_anagram.js
@@ -1,3 +1,35 @@
 const isAnagram = (strA, strB) => {
-    
+    let frequencyA = {};
+    let frequencyB = {};
+
+    if(strA.length !== strB.length) {
+        return false;
+    }
+
+    for(let i = 0; i < strA.length; i++) {
+        if(frequencyA[strA[i]] === undefined) {
+          frequencyA[strA[i]] = 1;
+        } else {
+          frequencyA[strA[i]] = frequencyA[strA[i]] + 1;
+        }
+    }
+    for(let j = 0; j < strB.length; j++) {
+        if(frequencyB[strB[j]] === undefined) {
+          frequencyB[strB[j]] = 1;
+        } else {
+          frequencyB[strB[j]] = frequencyB[strB[j]] + 1;
+        }
+    }
+    for(var key in frequencyA) {
+        if(frequencyA[key] !== frequencyB[key]) {
+          return false;
+        }
+    }
+
+    return true;
 }
+
+//isAnagram('cinema', 'iceman'); //true
+//isAnagram('tarp', 'part'); //true
+//isAnagram('carp', 'craps'); //false
+//isAnagram('car', 'cat'); //false
